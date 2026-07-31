c9955391865e03fe9ea51f81249f18a0aa17b898
xy <granostayshaniigh@gmail.com>
2026-07-31 22:10:28 +0800

    7.31
    2026.7.31

---
+++ src/oop_test1/Dog.java
@@ -0,0 +1,20 @@
+//类，下一步是创建对象
+
+package oop_test1;
+
+/**
+ * 狗类 - 用于表示狗的基本信息
+ */
+public class Dog {
+    /** 狗的名字 */
+    String name;
+    
+    /** 狗的年龄（单位：岁） */
+    int age;
+    
+    /** 狗的体重（单位：千克） */
+    double weight;
+    
+    /** 狗的颜色 */
+    String color;
+}
---
+++ src/oop_test1/test.java
@@ -0,0 +1,15 @@
+package oop_test1;
+
+public class test{
+    public static void main(String[] args){
+        Dog d1 = new Dog();
+        d1.name = "旺财";
+        d1.age = 3;
+        d1.weight = 3.5;
+        d1.color = "白色";
+        System.out.println(d1.name);
+        System.out.println(d1.age);
+        System.out.println(d1.weight);
+        System.out.println(d1.color);
+    }
+}
